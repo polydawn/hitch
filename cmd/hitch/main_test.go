77b872b1f54c7cebe64adf29fb9e37d0d405315f
Eric Myhre <hash@exultant.us>
2017-06-16 19:26:19 -0500

    cli: force consistent in/out/err; usage testable.
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- cmd/hitch/main_test.go
+++ cmd/hitch/main_test.go
@@ -30,6 +30,15 @@ func TestCLI(t *testing.T) {
 			So(stdout, ShouldEqual, "")
 			So(code, ShouldEqual, 1)
 		})
+		Convey("'hitch -h' should emit help", func() {
+			stdout, stderr, code := callMain(
+				[]string{"-h"}, nil,
+			)
+			So(stderr, ShouldStartWith, "usage: hitch")
+			So(stderr, ShouldNotContainSubstring, "missing subcommand")
+			So(stdout, ShouldEqual, "")
+			So(code, ShouldEqual, 0)
+		})
 		Convey("'hitch -x' should error about unknown flag", func() {
 			stdout, stderr, code := callMain(
 				[]string{"-x"}, nil,

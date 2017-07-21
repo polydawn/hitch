e204b727efc6a3901af1e2095396c1f81ba8b7a8
Eric Myhre <hash@exultant.us>
2017-07-20 20:34:08 -0500

    core: hitch.db is now a *dir*.
    The default staging state path will go *inside* that dir.  As will everything else.
    
    I believe this will tend closer to the overall usage semantics we want.  E.g., I have a "hacking" dir, and "hacking/proj1" and "hacking/proj2" share a hitch database... which makes sense to place as "hacking/hitch.db/*".
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- lib/locator/locator.go
+++ lib/locator/locator.go
@@ -26,6 +26,7 @@ func FindByFilename(startPath string, fileName string) (dirFound string, f *os.F
 		if err == nil {
 			return dirFound, f, nil
 		}
+		defer f.Close()
 		if os.IsNotExist(err) {
 			dirFound = filepath.Dir(dirFound)
 			if dirFound == "." || dirFound == fpSepStr {
@@ -51,6 +52,79 @@ func FindByFilenameFromCwd(fileName string) (dirFound string, f *os.File, erm er
 		return "", nil, meep.Meep(&ErrCantRead{}, meep.Cause(err))
 	}
 	return FindByFilename(cwd, fileName)
+}
+
+/*
+	Look for a workspace indicated by a directory with special name:
+	starting from `startPath`,
+	iterating up through parent directories,
+	looking for dirs in each parent dir named `dirName`,
+	and returning either the path found with that name, or error.
+
+	Symlinks to directories are also acceptable.
+
+	Errors:
+
+		- *ErrNotFound -- if we never find a dir named 'dirName'
+		- *ErrCantRead -- if any other IO errors iterating up the parent dirs for reading
+*/
+func FindByDirname(startPath string, dirName string) (string, error) {
+	dirFound := filepath.Clean(startPath)
+	if filepath.Base(dirFound) == dirName {
+		return dirFound, nil
+	}
+	for {
+		// `ls`.  Any errors: return.
+		f, err := os.Open(dirFound)
+		if err != nil {
+			if os.IsNotExist(err) {
+				return "", meep.Meep(&ErrNotFound{})
+			}
+			return "", meep.Meep(&ErrCantRead{}, meep.Cause(err))
+		}
+		defer f.Close()
+		// Scan through all entries in the dir, looking for our fav.
+		names, err := f.Readdirnames(-1)
+		if err != nil {
+			return "", meep.Meep(&ErrCantRead{}, meep.Cause(err))
+		}
+		for _, name := range names {
+			if name != dirName {
+				continue
+			}
+			dirFound := filepath.Join(dirFound, name)
+			fi, err := os.Stat(dirFound)
+			if err != nil {
+				return "", meep.Meep(&ErrCantRead{}, meep.Cause(err))
+			}
+			if !fi.IsDir() {
+				break
+			}
+			return dirFound, nil
+		}
+		// If basename'ing got us "/" this time, and we still didn't find it, terminate.
+		if dirFound == "/" || dirFound == "." {
+			return "", meep.Meep(&ErrNotFound{})
+		}
+		// Step up one dir.
+		dirFound = filepath.Dir(dirFound)
+	}
+}
+
+/*
+	As per FindByDirname, but the start dir is the process current working directory.
+
+	Errors:
+
+		- the errors from `FindByDirname()`
+		- *ErrCantRead -- if the current process CWD cannot be detected.
+*/
+func FindByDirnameFromCwd(dirName string) (dirFound string, err error) {
+	cwd, err := os.Getwd()
+	if err != nil {
+		return "", meep.Meep(&ErrCantRead{}, meep.Cause(err))
+	}
+	return FindByDirname(cwd, dirName)
 }
 
 type ErrNotFound struct {
